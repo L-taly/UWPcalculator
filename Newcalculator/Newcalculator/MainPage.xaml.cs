e0efb95a43a50d423a261a8cdce68b347821d4fa
L-taly <litanlinlitanlin@outlook.com>
2017-03-30 09:40:58 +0800

    operator，没有计算，

--- Newcalculator/Newcalculator/MainPage.xaml.cs
+++ Newcalculator/Newcalculator/MainPage.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Data;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -17,7 +16,7 @@ using Windows.UI.Xaml.Navigation;
 
 // https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
 
-namespace Calculator
+namespace Newcalculator
 {
     /// <summary>
     /// 可用于自身或导航至 Frame 内部的空白页。
@@ -34,11 +33,12 @@ namespace Calculator
         public bool Operator;
         public bool Point = true;
 
-        //private string Get_Result(string Operations)
-        //{
-            
-            
-        //}
+        private string Get_Result(string Operations)
+        {
+            DataTable table = new DataTable().compute(Operations,"");
+            string numss = table.ToString();
+            return table;
+        }
 
         //private void Button_Click_Operator(object sender,RoutedEventArgs e)
         //{
@@ -66,22 +66,8 @@ namespace Calculator
                 }
                 InputBox.Text = InputText;
             }
-            //OutputBox.Text = Get_Result(OutputBox.Text);
+            OutputBox.Text = Get_Result(OutputBox.Text);
             AbleToAddop = true;
-        }
-
-        private void Button_Click_Operator(object sender, RoutedEventArgs e)
-        {
-            Button tempBTN = (Button)sender;
-            if(OutputText != "Error!")
-            {
-                if (AbleToAddop)
-                {
-                    InputText += tempBTN.Content.ToString();
-                }
-                InputBox.Text = InputText;
-            }
-            AbleToAddop = false;
         }
 
         private void Button_Click_Num(object sender, RoutedEventArgs e)
@@ -103,11 +89,7 @@ namespace Calculator
             {
                 OutputBox.Text = "Please Clear it.";
             }
-            //OutputBox.Text = Get_Result(OutputBox.Text);
-            if (!AbleToAddop)
-            {
-                Point = true;
-            }
+            OutputBox.Text = Get_Result(OutputBox.Text);
             AbleToAddop = true;
         }
     }
